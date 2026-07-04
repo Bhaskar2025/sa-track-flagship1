df633e626f2649317d99d93898fc46b5657a9d0d
bnaruka <bhaskar.naruka@gmail.com>
2026-07-05 00:51:02 +0530

    ci: add /hello endpoint to prove the pipeline

---
+++ src/main/java/com/bnaruka/sa_flagship/HelloController.java
@@ -0,0 +1,13 @@
+package com.bnaruka.sa_flagship;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "D6 - Pipeline is live";
+    }
+}
